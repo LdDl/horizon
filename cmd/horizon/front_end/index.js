544a66ab6009914941f536696ccbe7de1e882498
Dimitrii <sexykdi@gmail.com>
2020-03-12 11:09:10 +0300

    prepare request data with FE

--- cmd/horizon/front_end/index.js
+++ cmd/horizon/front_end/index.js
@@ -6,6 +6,41 @@ var map = new mapboxgl.Map({
     zoom: 1
 });
 
+var draw = new MapboxDraw({
+    displayControlsDefault: false,
+    controls: {
+        point: true,
+        trash: true
+    }
+});
+
+map.addControl(draw, 'top-left');
+
 map.on('load', function() {
     console.log("Map has been loaded");
+    map.on('draw.create', updateMapMatch);
+    map.on('draw.delete', updateMapMatch);
+    map.on('draw.update', updateMapMatch);
 });
+
+function updateMapMatch(e) {
+    var data = draw.getAll();
+    if (data.features.length < 3) {
+        console.log(`You need to provide another ${3-data.features.length} GPS points`);
+        return
+    }
+    console.log("Doing map matching");
+
+    let gpsMeasurements = data.features.map(element => {
+       return {
+           "tm": element.id,
+           "lonLat": [element.geometry.coordinates[0], element.geometry.coordinates[0]],
+       };
+    });
+    
+    let requestData = {
+        "maxStates": 5,
+        "stateRadius": 7,
+        "gps": gpsMeasurements
+    }
+}
