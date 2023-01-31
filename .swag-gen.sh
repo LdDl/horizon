380ac3ab3dce638ba8276c4155de8eec8a95870f
Dimitrii <sexykdi@gmail.com>
2023-01-31 17:54:11 +0300

    gen swag upd

--- .swag-gen.sh
+++ .swag-gen.sh
@@ -1,4 +1,4 @@
-swag init --generalInfo cmd/horizon/main.go --output rest/docs
-rm rest/docs/docs.go
-rm rest/docs/swagger.yaml
+swag init --generalInfo cmd/horizon/main.go --output rest/docs && \
+rm rest/docs/docs.go && \
+rm rest/docs/swagger.yaml && \
 mv rest/docs/swagger.json rest/docs/assets/swagger.json
